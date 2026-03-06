4794bb9ee3b094993173d053de0629aa7da0734b
Ailson Brito <102686695+BritoAilson@users.noreply.github.com>
2026-03-06 08:07:18 -0300

    Arquivo para teste em changes

---
+++ Arquivo_teste_c.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Biblioteca stdio.h configurada com sucesso!\n");
+    return 0;
+}
--- Modificadores.c
+++ Modificadores.c
@@ -35,11 +35,11 @@ int main() {
 
    
  
-int main() {
-    unsigned long int largePositiveNumber = 4000000000;
-    printf("Número positivo grande: %lu\n", largePositiveNumber);
+//int main() {
+    //unsigned long int largePositiveNumber = 4000000000;
+    //printf("Número positivo grande: %lu\n", largePositiveNumber);
  
-    return 0;
+    //return 0;
 
 
 

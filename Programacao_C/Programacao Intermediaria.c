04ac0baf0b32c19e5f5f8af164bdf78e6e76510f
Ailson Brito <102686695+BritoAilson@users.noreply.github.com>
2026-03-09 08:55:16 +0000

    Programacao intermediaria arvoro de decisao

--- Programacao_C/Programacao Intermediaria.c
+++ Programacao_C/Programacao Intermediaria.c
@@ -1,11 +1,37 @@
 #include <stdio.h>
- 
-// Função genérica para entrada de dados
-//void entradaDados() {
-    // código para a função entradaDados
-}
- 
-// Função genérica para cálculo da média
-//float calcularMedia() {
-    // código para a função calcularMedia
+
+int main () {
+//** Estudo da Estrutura de Decisão estrutura Sequencial**/
+
+    //int maca, laranja;
+
+   // printf("Digite a quantidade de maças: ");
+   // scanf("%d", &maca);
+    
+   // printf("Digite a quantidade de laranjas: ");
+   // scanf("%d", &laranja);
+
+
+   // if (maca <= laranja ) {
+
+       // printf("Tem mais laranjas do que maças.\n");
+
+   // } else if (maca < 0) {
+      //  printf("Tem um numero negativo de maças, o que e invalido.\n");
+   // } else if (laranja <= maca) {
+     //   printf("Tem mais maças do que laranjas.\n");
+    //} else {
+      ///  printf("Tem a mesma quantidade de maças e laranjas.\n");
+   // }
+
+   // return 0;
+
+   
+
+
+
+
+
+
+
 }

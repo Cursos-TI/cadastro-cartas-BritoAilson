5d26e579fcff64bed508115b2140180c35ec7ead
Ailson Brito <102686695+BritoAilson@users.noreply.github.com>
2026-03-13 09:03:22 +0000

    Estrutura de Decisão

---
+++ Programacao_C/Programacao_C_Estrutura_Decisao.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+//int main() {
+  //int numero1 = 8;
+  //int numero2 = 5;
+
+  //if (numero1 > numero2) {
+    //printf("numero1 é maior que numero2\n");
+  //}
+
+  //return 0;
+//}
+
+//int main() {
+  //int idade = 25;
+ 
+
+  //if (idade >= 20) {
+    //printf("idade é maior que 20\n");
+    //printf("Flag, Dentro IF\n");
+  //}
+//printf("Flag, Fora IF\n");
+  //return 0;
+//}
+
+//int main () {
+//float  temperatura;
+//printf("Digite a temperatura: \n", temperatura);
+//scanf("%f", &temperatura);
+
+//if (temperatura > 30.0) {
+  //printf("Está quente!\n");
+//} else {
+  //printf("Está frio!\n");
+
+//}
+//return 0;
+//}
+
+int main() {
+  int numero = 4;
+
+  if (numero % 2 == 0) {
+    printf("O número é par.\n");
+  } else {
+    printf("O número é ímpar.\n");
+  }
+
+  return 0;
+}
